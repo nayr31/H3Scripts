ca3c773d154bd85fe3a09deb86f2639c13d739e3
Ryan Epp <re17wj@brocku.ca>
2021-08-11 19:59:33 -0400

    init

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace H3VR_Stuff
-{
-    public class Class1
-    {
-    }
-}
---
+++ bin/Release/net35/RotateToPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using FistVR;
+
+namespace H3VR_Stuff
+{
+    public class RotateToPlayer : MonoBehaviour
+    {
+        public GameObject Item;
+
+        public void Start()
+        {
+            Transform pPos = GM.CurrentPlayerBody.Head.transform;
+
+            Vector3 v3Dir = Item.transform.position - pPos.position;
+            float angle = Mathf.Atan2(v3Dir.x, v3Dir.z) * Mathf.Rad2Deg;
+            Item.transform.eulerAngles = new Vector3(0, angle, 0);
+        }
+    }
+}
